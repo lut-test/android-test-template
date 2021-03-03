8dd7e8e04c4ce0434052026c7561af63fdbcfdef
petterigit <33552590+petterigit@users.noreply.github.com>
2021-03-03 13:08:17 +0200

    remove --skin

--- global_setup_no_sudo.sh
+++ global_setup_no_sudo.sh
@@ -42,6 +42,8 @@ export ANDROID_HOME=/usr/local/lib/android/sdk
 #echo y | ${ANDROID_HOME}/tools/android -s update sdk --all --force --no-ui --filter android-$AVD_VERSION,sys-img-x86-google_apis-$AVD_VERSION > /dev/null 
 #
 
+$ANDROID_HOME/tools/emulator -list-avds
+
 echo y | ${ANDROID_HOME}/tools/android -s create avd --force --name android-$AVD_VERSION \
-  --device "Nexus S" --name "nexus" --abi "google_apis/x86" --skin WVGA800 >/dev/null
-#  --device "Nexus S" --name "nexus" --abi "default/x86" --skin WVGA800
+  --device "Nexus S" --name "nexus" --abi "google_apis/x86" >/dev/null
+  #--device "Nexus S" --name "nexus" --abi "default/x86" --skin WVGA800
