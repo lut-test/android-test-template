cb87c416e5d7b280c71e760ef33f61b94d0ca1af
petterigit <33552590+petterigit@users.noreply.github.com>
2021-03-03 15:40:33 +0200

    a

--- run.sh
+++ run.sh
@@ -24,9 +24,9 @@ $ANDROID_HOME/platform-tools/adb start-server
 
 #$ANDROID_HOME/platform-tools/bin/emulator -list-avds
 
-echo 'tools'
-ls $ANDROID_HOME/tools/
-./$ANDROID_HOME/tools/emulator -avd Nexus_10 -netdelay none -netspeed full &
+echo 'tools list:'
+ls $ANDROID_HOME/tools/emulator
+$ANDROID_HOME/tools/emulator -avd Nexus_10 -netdelay none -netspeed full &
 
 server_pid=$!
 output=''
